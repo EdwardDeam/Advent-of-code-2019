76dd510b0ac630e2b977f5ca74a78b308bd57579
Edward Deam <edwardcrdeam@gmail.com>
2019-12-02 11:05:23 +1100

    Day one Part one

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Advent of Code 2019
+## Edward Deam
+C# solutions to the 2019 Advent of Code.
+
+
+ - [Day 1](day1/day1.md) - The Tyranny of the Rocket Equation.
---
+++ day1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace DayOne
+{
+    class RocketModule
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                if(args == null || args.Length == 0)
+                {
+                    Console.WriteLine("Program requires Input File as an Argument.");
+                    throw new ArgumentException("No Input File Specified.");
+                }
+
+                int fuelCount = 0;
+                StreamReader sr = File.OpenText(args[0]);
+                {   
+                    string s;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        fuelCount += FuelRequirement(System.Convert.ToInt32(s));
+                    }
+                }
+
+                Console.WriteLine("Total Fuel Required: " + fuelCount.ToString());
+            }
+            catch (Exception e )
+            {
+                Console.WriteLine("Error in Main: " + e.Message);
+            }
+        }
+        
+        public static int FuelRequirement(int mass)
+        {
+            return (int)(MathF.Floor(mass / 3.0f) - 2.0f);
+        }
+    }
+}
