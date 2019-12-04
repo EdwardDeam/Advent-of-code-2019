51b612429804c183f717e0b55a97ce823002a664
Edward Deam <edwardcrdeam@gmail.com>
2019-12-04 22:33:29 +1100

    Gets interecting Points

--- day3/Program.cs
+++ day3/Program.cs
@@ -14,19 +14,21 @@ namespace DayThree
         Console.WriteLine("Program requires Input File as an Argument.");
         return;
       }
-      List<List<Coord>> lines = LoadFile(args[0]);
-      foreach (var line in lines)
-      {
-        System.Console.WriteLine("\n\n");
-          foreach(var coord in line){
-            System.Console.WriteLine(coord.ToString());
-          }
+      List<List<Coord>> paths = GeneratePathFromFile(args[0]);
+      // foreach (var line in paths)
+      // {
+      //   System.Console.WriteLine("\n\n");
+      //     foreach(var coord in line){
+      //       System.Console.WriteLine(coord.ToString());
+      //     }
+      // }
+      List<Coord> intersections = FindIntersections(paths);
+      foreach(var coord in intersections){
+        System.Console.WriteLine(coord.ToString() );
       }
-      
-
     }
 
-    static List<List<Coord>> LoadFile(string path)
+    static List<List<Coord>> GeneratePathFromFile(string path)
     {
       List<List<Coord>> results = new List<List<Coord>>();
       StreamReader sr = File.OpenText(path);
@@ -53,49 +55,122 @@ namespace DayThree
         GroupCollection groups = m.Groups;
         switch (groups[1].Value)
         {
-            case "R":
+          case "R":
             {
-            Coord newCord = new Coord(currentCoord.x + System.Convert.ToInt32(groups[2].Value), currentCoord.y);
-            results.Add(newCord);
-            currentCoord = newCord;
-            break;
+              Coord newCord = new Coord(currentCoord.x + System.Convert.ToInt32(groups[2].Value), currentCoord.y);
+              results.Add(newCord);
+              currentCoord = newCord;
+              break;
             }
-            case "L":
+          case "L":
             {
-            Coord newCord = new Coord(currentCoord.x - System.Convert.ToInt32(groups[2].Value), currentCoord.y);
-            results.Add(newCord);
-            currentCoord = newCord;
-            break;
+              Coord newCord = new Coord(currentCoord.x - System.Convert.ToInt32(groups[2].Value), currentCoord.y);
+              results.Add(newCord);
+              currentCoord = newCord;
+              break;
             }
-            case "U":
+          case "U":
             {
-            Coord newCord = new Coord(currentCoord.x, currentCoord.y + System.Convert.ToInt32(groups[2].Value));
-            results.Add(newCord);
-            currentCoord = newCord;
-            break;
+              Coord newCord = new Coord(currentCoord.x, currentCoord.y + System.Convert.ToInt32(groups[2].Value));
+              results.Add(newCord);
+              currentCoord = newCord;
+              break;
             }
-            case "D":
+          case "D":
             {
-            Coord newCord = new Coord(currentCoord.x, currentCoord.y -System.Convert.ToInt32(groups[2].Value));
-            results.Add(newCord);
-            currentCoord = newCord;
-            break;
+              Coord newCord = new Coord(currentCoord.x, currentCoord.y - System.Convert.ToInt32(groups[2].Value));
+              results.Add(newCord);
+              currentCoord = newCord;
+              break;
             }
         }
       }
       return results;
     }
+    public static List<Coord> FindIntersections(List<List<Coord>> paths)
+    {
+      List<Coord> intersections = new List<Coord>();
+      List<Coord> pathA = paths[0];
+      List<Coord> pathB = paths[1];
+      Coord previous1 = pathA[0];
+      Coord previous2 = pathB[0];
+      for (int i = 1; i < pathA.Count; i++)
+      {
+        Coord current1 = pathA[i];
+        for (int j = 1; j < pathB.Count; j++)
+        {
+          Coord current2 = pathB[j];
+          if(CompareLines(previous1, current1, previous2, current2))
+          {
+            intersections.Add(GetIntersectingCoord(previous1, current1, previous2, current2));
+          }
+          previous2 = current2;
+        }
+        previous1 = current1;
+      }
+
+      return intersections;
+    }
+    private static Coord GetIntersectingCoord(Coord a1, Coord a2, Coord b1, Coord b2)
+    {
+      int dax = a1.x - a2.x; // Line A is Vertical
+      if (dax == 0)
+      {
+        return new Coord(a1.x, b1.y);
+      }
+      else // Line B is Horizontal
+      {
+        return new Coord(b1.x, a1.y);
+      }
+    }
+    private static bool CompareLines(Coord a1, Coord a2, Coord b1, Coord b2)
+    {
+      // Decide the Direction of line A. If the delta X is 0 then the line is vertical.
+      int dax = a1.x - a2.x;
+      int dbx = b1.x - b2.x;
+      // Assume that the lines dont run parallel for now.
+      if (dax == 0 && dbx == 0 || dax != 0 && dbx != 0)
+      {
+        return false;
+      }
+      bool testX, testY;
+      if (dax == 0) // Line A is Vertical
+      {
+        testX = InRange(a1.x, b1.x, b2.x);
+        testY = InRange(b1.y, a1.y, a2.y);
+      }
+      else // Line A is Horizontal
+      {
+        testX = InRange(b1.x, a1.x, a2.x);
+        testY = InRange(a1.y, b1.y, b2.y);
+      }
+      return testX && testY;
+    }
+
+    // Check if a value is in a range, compensates for reversed min,max values
+    private static bool InRange(int v, int a, int b)
+    {
+      if (a > b)
+      {
+        return v >= b && v <= a;
+      }
+      else
+      {
+        return v >= a && v <= b;
+      }
+    }
   }
-}
-public struct Coord
-{
-  public int x, y;
-  public Coord(int _x, int _y)
+  public struct Coord
   {
-    x = _x;
-    y = _y;
-  }
-  public override string ToString() {
-    return "X: " + x.ToString() + ", Y: " + y.ToString();
+    public int x, y;
+    public Coord(int _x, int _y)
+    {
+      x = _x;
+      y = _y;
+    }
+    public override string ToString()
+    {
+      return "X: " + x.ToString() + ", Y: " + y.ToString();
+    }
   }
 }
