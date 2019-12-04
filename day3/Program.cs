b937354afbeb17a4843d255dfc1252325dbc081e
Edward Deam <edwardcrdeam@gmail.com>
2019-12-04 23:18:11 +1100

    Part One Done!

--- day3/Program.cs
+++ day3/Program.cs
@@ -15,17 +15,21 @@ namespace DayThree
         return;
       }
       List<List<Coord>> paths = GeneratePathFromFile(args[0]);
+      // System.Console.WriteLine("PATHS");
       // foreach (var line in paths)
       // {
-      //   System.Console.WriteLine("\n\n");
+      //   System.Console.WriteLine("\n");
       //     foreach(var coord in line){
       //       System.Console.WriteLine(coord.ToString());
       //     }
       // }
       List<Coord> intersections = FindIntersections(paths);
-      foreach(var coord in intersections){
-        System.Console.WriteLine(coord.ToString() );
-      }
+      // System.Console.WriteLine("\nINTERSECTIONS");
+      // foreach(var coord in intersections){
+      //   System.Console.WriteLine(coord.ToString() );
+      // }
+      int answer = ShortestMahattan(intersections);
+      System.Console.WriteLine("\nThe Shorteset Distance is: " + answer.ToString());
     }
 
     static List<List<Coord>> GeneratePathFromFile(string path)
@@ -41,7 +45,7 @@ namespace DayThree
       }
       return results;
     }
-    static List<Coord> GenerateCoordList(string input)
+    private static List<Coord> GenerateCoordList(string input)
     {
       List<Coord> results = new List<Coord>();
       string[] data = input.Split(',');
@@ -87,22 +91,24 @@ namespace DayThree
       }
       return results;
     }
-    public static List<Coord> FindIntersections(List<List<Coord>> paths)
+
+    private static List<Coord> FindIntersections(List<List<Coord>> paths)
     {
       List<Coord> intersections = new List<Coord>();
       List<Coord> pathA = paths[0];
       List<Coord> pathB = paths[1];
       Coord previous1 = pathA[0];
-      Coord previous2 = pathB[0];
       for (int i = 1; i < pathA.Count; i++)
       {
         Coord current1 = pathA[i];
+        Coord previous2 = pathB[0];
         for (int j = 1; j < pathB.Count; j++)
         {
           Coord current2 = pathB[j];
           if(CompareLines(previous1, current1, previous2, current2))
           {
-            intersections.Add(GetIntersectingCoord(previous1, current1, previous2, current2));
+            var t = GetIntersectingCoord(previous1, current1, previous2, current2);
+            intersections.Add(t);
           }
           previous2 = current2;
         }
@@ -111,18 +117,7 @@ namespace DayThree
 
       return intersections;
     }
-    private static Coord GetIntersectingCoord(Coord a1, Coord a2, Coord b1, Coord b2)
-    {
-      int dax = a1.x - a2.x; // Line A is Vertical
-      if (dax == 0)
-      {
-        return new Coord(a1.x, b1.y);
-      }
-      else // Line B is Horizontal
-      {
-        return new Coord(b1.x, a1.y);
-      }
-    }
+
     private static bool CompareLines(Coord a1, Coord a2, Coord b1, Coord b2)
     {
       // Decide the Direction of line A. If the delta X is 0 then the line is vertical.
@@ -147,6 +142,38 @@ namespace DayThree
       return testX && testY;
     }
 
+    private static Coord GetIntersectingCoord(Coord a1, Coord a2, Coord b1, Coord b2)
+    {
+      int dax = a1.x - a2.x; // Line A is Vertical
+      if (dax == 0)
+      {
+        return new Coord(a1.x, b1.y);
+      }
+      else // Line B is Horizontal
+      {
+        return new Coord(b1.x, a1.y);
+      }
+    }
+
+    private static int ShortestMahattan(List<Coord> coords)
+    {
+      // Coord shortest = new Coord(0,0);
+      int shortest = int.MaxValue;
+      foreach(Coord c in coords) {
+        // Ignore the intersection at the origin.
+        if(c.x == 0 && c.y == 0){
+          continue;
+        }
+        // Get Absolute values
+        int x = c.x > 0 ? c.x : -1 * c.x;
+        int y = c.y > 0 ? c.y : -1 * c.y;
+        int distance = x + y;
+        if(distance < shortest) {
+          shortest = distance;
+        }
+      }
+      return shortest;
+    }
     // Check if a value is in a range, compensates for reversed min,max values
     private static bool InRange(int v, int a, int b)
     {
